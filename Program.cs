5cd5db4b409ccc06315a051122da921209a7ca86
Shane Macallister <shanemacallister@gmail.com>
2020-03-12 20:18:20 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringReverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Takes user input, reverses it and prints the result to the console.
+             */
+            string line;
+            Console.WriteLine("Enter xx to exit or any key to continue.");
+            while ((line = Console.ReadLine()) != "xx")
+            {
+                Console.WriteLine("Enter whatever you want reversed:");
+                string reverseMe = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(reverseMe))
+                {
+                    string reversed = Reverse(reverseMe);
+                    Console.WriteLine(reversed);
+                    Console.WriteLine("Enter xx to exit or any key to continue.");
+                }
+                else
+                {
+                    Console.WriteLine("You didn't enter anything!");
+                }
+            }
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
